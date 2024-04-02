2049e61b5cecfa6bb82e52ee9c2e4687eab49473
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-04-02 23:33:08 +0700

    jpa paging project initialized

---
+++ JPA paging/JPA-paging/src/main/java/xyz/htoomaungthait/JPA/paging/JpaPagingApplication.java
@@ -0,0 +1,13 @@
+package xyz.htoomaungthait.JPA.paging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPagingApplication.class, args);
+	}
+
+}
